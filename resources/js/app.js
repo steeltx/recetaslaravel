d6b2a5e60c0c4991ea275f5afcc412dcab467181
steeltx <realsteel.gnu@gmail.com>
2021-11-11 19:42:55 -0600

    Integrando OwlCarousel

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,6 +5,7 @@
  */
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
+import 'owl.carousel';
 
 require('./bootstrap');
 
@@ -37,6 +38,22 @@ const app = new Vue({
     el: '#app',
 });
 
-$('.like-btn').on('click', function() {
-    $(this).toggleClass('like-active');
- });
+jQuery(document).ready(function(){
+    jQuery('.owl-carousel').owlCarousel({
+        margin: 10,
+        loop: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+            0 : {
+                items: 1
+            },
+            600:{
+                items: 2
+            },
+            1000: {
+                items: 3
+            }
+        }
+    });
+});
